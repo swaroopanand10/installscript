c10152eed3c618ddc31c56c1619445b075d2910b
swaroopanand <swaroopanand695@gmail.com>
2023-04-17 21:32:09 +0530

    first commit

---
+++ installscript.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+pacman -S --noconfirm --needed base-devel 
+
+git clone https://aur.archlinux.org/yay.git
+chown -R  cloudcone:users yay
+cd yay; makepkg -si
+
+yay -S --noconfirm paru
+paru --noconfirm --needed -S - < ./packages.txt
+
+echo "[user]
+	name = swaroopanand
+	email = swaroopanand@gmail.com
+[init]
+	defaultBranch = main
+[color]
+	ui = auto
+[core]
+	excludeFile = ~/.gitignore">.gitconfig
+
+
+# setting up git bare repo
+git clone --bare git@github.com:swaroopanand10/.cfg.git $HOME/.cfg
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
+
+
+git clone git@github.com:swaroopanand10/nvim.git
